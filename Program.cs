e5fbeab71312cf7c8a2c5224a943d9d27d60a595
Alex Sendre <rares.sendre@gmail.com>
2024-02-24 09:39:10 -0600

    get request for viewing order history

--- Program.cs
+++ Program.cs
@@ -31,34 +31,45 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// get all users
 app.MapGet("/api/users", (BangazonDbContext db) =>
 {
     return db.Users.ToList();
 });
 
+// get all products
 app.MapGet("/api/products", (BangazonDbContext db) =>
 {
     return db.Products.ToList();
 });
 
+// get all categories
 app.MapGet("/api/categories", (BangazonDbContext db) =>
 {
     return db.Categories.ToList();
 });
 
+// get specific seller products
 app.MapGet("/api/{sellerId}/products", (BangazonDbContext db, int sellerId) =>
 {
-    return db.Products.Single(u => u.SellerId == sellerId);
+    return db.Products.Where(u => u.SellerId == sellerId);
 });
 
+// get product details
 app.MapGet("/api/products/{productId}", (BangazonDbContext db, int productId) =>
 {
     return db.Products.Single(p => p.Id == productId);
 });
 
+// get complete orders
 app.MapGet("/api/orders/complete", (BangazonDbContext db) =>
 {
     return db.Orders.Where(o => o.IsComplete).ToList();
+});
+
+app.MapGet("/api/orders/{id}/history", (BangazonDbContext db, int id) =>
+{
+    return db.Orders.Where(u => u.CustomerId == id).ToList();
 });
 
 app.Run();
