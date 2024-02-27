7d8e58c3efdcc808837243dc3737660f504e59e6
Alex Sendre <rares.sendre@gmail.com>
2024-02-26 19:38:44 -0600

    create dto class for user

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BangazonBE.DTOs
+{
+    public class UserDTO
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsSeller { get; set; }
+        public string Uid { get; set; }
+    }
+}
